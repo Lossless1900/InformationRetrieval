63ca4da4ff12278210e53420529f5abedaf1e773
Xingyu <xingyu1988@gmail.com>
2014-02-14 22:17:43 -0500

    Update BingTest.java

--- src/BingTest.java
+++ src/BingTest.java
@@ -1,5 +1,5 @@
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.BufferedReader; 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
